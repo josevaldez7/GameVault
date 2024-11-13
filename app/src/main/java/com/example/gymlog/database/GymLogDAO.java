89a2266f9df3d6bc90497052c2580d97daccca82
Jose Valdez <jvaldez-fernandez@csumb.edu>
2024-11-12 20:34:11 -0800

    End of Video 6

--- app/src/main/java/com/example/gymlog/database/GymLogDAO.java
+++ app/src/main/java/com/example/gymlog/database/GymLogDAO.java
@@ -15,6 +15,6 @@ public interface GymLogDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(GymLog gymLog);
 
-    @Query("Select * from " + GymLogDataBase.GYM_LOG_TABLE)
+    @Query("SELECT * FROM " + GymLogDataBase.GYM_LOG_TABLE + " ORDER BY date DESC")
     List<GymLog> getAllRecords();
 }
